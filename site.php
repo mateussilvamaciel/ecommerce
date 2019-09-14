a0b4b403774aa84c85346bdcdb632ceb927f0832
mateus <mateussilvamaciel@gmail.com>
2019-09-14 14:19:38 -0300

    Integraçaao com PayPal HTML

--- site.php
+++ site.php
@@ -257,7 +257,17 @@ $app->post("/checkout", function(){
 
 	$order->save();
 
-	header("Location: /order/".$order->getidorder()."/pagseguro");
+	switch ((int)$_POST['payment-method']) {
+
+		case 1:
+		header("Location: /order/".$order->getidorder()."/pagseguro");
+		break;
+
+		case 2:
+		header("Location: /order/".$order->getidorder()."/paypal");
+		break;
+
+	}
 	exit;
 
 });
@@ -285,6 +295,28 @@ $app->get("/order/:idorder/pagseguro", function($idorder){
 			'areaCode'=>substr($order->getnrphone(), 0, 2),
 			'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
 		]
+	]);
+});
+
+$app->get("/order/:idorder/paypal", function($idorder){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+	$page->setTpl("payment-paypal", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts(),
 	]);
 });
 
